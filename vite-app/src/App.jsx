9f32f8065054190cc570e2559855e41879ed968c
Ali Siddiqui <alisiddiquia22@yahoo.com>
2023-06-11 18:15:13 -0500

    updated

---
+++ vite-app/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Header from "./Header";
+
+function App() {
+  return (
+    <>
+    <Header />
+    <h1>hi</h1>
+    </>
+  )
+}
+
+export default App
---
+++ vite-app/src/Header.jsx
@@ -0,0 +1,15 @@
+import Button from 'react-boostrap/Button'
+import 'bootstrap/dist/css/bootrstap.min.css'
+
+
+function Header(){
+    return (
+        <>
+        <Button>Click</Button>
+        <h1>Nerv</h1>
+        </>
+    )
+}
+
+
+export default Header
