e015881d3c10a63c336c450debb6f6e3c2f3e034
Ali Siddiqui <alisiddiquia22@yahoo.com>
2023-06-13 15:28:38 -0500

    finished login page and functionality

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require(`express`)
 const routes = require(`./routes/AppRouter`)
 const db = require(`./db`)
 const logger = require(`morgan`)
+const cors = require(`cors`)
 
 const PORT = process.env.PORT || 3001
 const app = express()
 
 app.use(express.json())
 app.use(logger(`dev`))
+app.use(cors())
 
 app.use(express.static(`./vite-app/dist`))
 // app.use(express.static(`./vite-app`))
